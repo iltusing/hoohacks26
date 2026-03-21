14fffb582631879d0e97c9805f179d0beb71f016
lexzach <github@zachbowden.com>
2026-03-21 16:55:14 -0400

    Add button scripts

---
+++ wild-west-game/scripts/title.gd
@@ -0,0 +1,16 @@
+extends Node
+
+func _on_ready() -> void:
+	%MAIN.visible = true;
+	%CREDITS.visible = false;
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_credits_pressed() -> void:
+	%MAIN.visible = false;
+	%CREDITS.visible = true;
+
+func _on_back_pressed() -> void:
+	%MAIN.visible = true;
+	%CREDITS.visible = false;
