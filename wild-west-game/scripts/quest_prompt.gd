c07f90723ea261845699e714027b20b238d610a8
Isabel <iltusing@wm.edu>
2026-03-22 04:26:38 -0400

    display quest text
    the player can trigger the quest text to show by walking into the area 2d im so tired

---
+++ wild-west-game/scripts/quest_prompt.gd
@@ -0,0 +1,16 @@
+extends CanvasGroup
+@onready var rich_text_label: RichTextLabel = $RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_interaction_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		rich_text_label.visible = true
