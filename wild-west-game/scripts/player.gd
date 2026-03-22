e80bb76f4a36918990e1a3bbbb146e13aaf1f5af
lexzach <github@zachbowden.com>
2026-03-21 23:11:51 -0400

    Add shooting/reloading animation logic

--- wild-west-game/scripts/player.gd
+++ wild-west-game/scripts/player.gd
@@ -6,6 +6,20 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+enum ActionState {
+	NONE,
+	SHOOTING,
+	RELOADING,
+}
+
+var action_state: ActionState = ActionState.NONE
+
+func _ready() -> void:
+	animated_sprite_2d.sprite_frames.set_animation_loop("shoot", false)
+	animated_sprite_2d.sprite_frames.set_animation_loop("reload", false)
+	animated_sprite_2d.animation_finished.connect(_on_animation_finished)
+	animated_sprite_2d.play("idle")
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -15,6 +29,11 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
+	
+	if Input.is_action_just_pressed("shoot") and action_state == ActionState.NONE:
+		action_state = ActionState.SHOOTING
+		animated_sprite_2d.play("shoot")
+
 
 	move_and_slide()
 	update_visuals()
@@ -32,7 +51,10 @@ func update_visuals():
 		animated_sprite_2d.flip_h = false
 	elif velocity.x < 0:
 		animated_sprite_2d.flip_h = true
-		
+	
+	if action_state != ActionState.NONE:
+		return
+
 	if not is_on_floor():
 		if velocity.y < 0:
 			animated_sprite_2d.play("jump")
@@ -42,3 +64,11 @@ func update_visuals():
 		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("idle")
+
+func _on_animation_finished() -> void:
+	if animated_sprite_2d.animation == "shoot":
+		action_state = ActionState.RELOADING
+		animated_sprite_2d.play("reload")
+	elif animated_sprite_2d.animation == "reload":
+		action_state = ActionState.NONE
+		update_visuals()
